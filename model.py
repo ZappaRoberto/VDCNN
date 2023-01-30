8728d9b1b556458ed00fa6c2f64417f94c406a9c
Roberto Zappa <roberto.zappa01@universitadipavia.it>
2023-01-30 12:43:04 +0100

    version 1.2

--- model.py
+++ model.py
@@ -1,53 +1,38 @@
 import torch
 from torch import nn
+import random
 from torchinfo import summary
-# TODO: understand embedding dimension. Is not clear
 
 
 class LookUpTable(nn.Module):
-    def __init__(self, f0=69, s=16):
+    def __init__(self, num_embedding, embedding_dim):
         super(LookUpTable, self).__init__()
-        '''
-        Our model begins with a look-up table that generates a 2D tensor of size (f0, s) that contain the embeddings
-        of the s characters. s is fixed to 1024, and f0 can be seen as the ”RGB” dimension of the input text.
-        '''
-        self.embeddings = nn.Embedding(f0, s)
+        self.embeddings = nn.Embedding(num_embedding, embedding_dim)
 
     def forward(self, x):
-        return self.embeddings(x)
+        return self.embeddings(x).transpose(1, 2)
 
 
 class FirstConvLayer(nn.Module):
-    def __init__(self, skip_connection, want_shortcut):
+    def __init__(self, in_channels, out_channels, kernel_size, skip_connection):
         super(FirstConvLayer, self).__init__()
         '''
         We first apply one layer of 64 convolutions of size 3
         '''
         self.sequential = nn.Sequential(
-            nn.Conv1d(in_channels=3, out_channels=64, kernel_size=3),
+            nn.Conv1d(in_channels, out_channels, kernel_size),
         )
-        self.want_shortcut = want_shortcut
-        if self.want_shortcut:
-            self.skip = skip_connection
-            self.sequential.append(self.skip)
+        if skip_connection is not None:
+            self.sequential.append(skip_connection)
 
     def forward(self, x):
-        if self.want_shortcut:
-            self.skip.clean()
         return self.sequential(x)
 
 
 class ConvolutionalBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, skip_connection, want_shortcut):
+    def __init__(self, in_channels, out_channels, skip_connection):
         super(ConvolutionalBlock, self).__init__()
-        '''
-         Each convolutional block is a sequence of two convolutional layers, 
-         each one followed by a temporal BatchNorm layer and an ReLU activation.
-         The kernel size of all the temporal convolutions is 3, with padding such that the temporal resolution is
-         preserved (or halved in the case of the convolutional pooling with stride 2)
-        '''
-        self.want_shortcut = want_shortcut
-        if self.want_shortcut:
+        if skip_connection is not None:
             self.skip_connection = skip_connection
             self.shortcut = nn.Sequential(
                 nn.Conv1d(in_channels * 2, out_channels, kernel_size=1, stride=2, bias=False),
@@ -64,7 +49,7 @@ class ConvolutionalBlock(nn.Module):
         )
 
     def forward(self, x):
-        if self.want_shortcut:
+        if self.skip_connection is not None:
             short = self.skip.get()
             out = self.sequential(x)
             if out.shape != short.shape:
@@ -109,7 +94,7 @@ class FullyConnectedBlock(nn.Module):
 
 
 class VDCNN(nn.Module):
-    def __init__(self, depth, want_shortcut=False):
+    def __init__(self, depth, want_shortcut=True):
         super(VDCNN, self).__init__()
         channels = [64, 128, 256, 512]
         if depth == 9:
@@ -121,20 +106,27 @@ class VDCNN(nn.Module):
         else:
             num_conv_block = [8, 8, 5, 3]
 
-        self.skip_connection = SkipConnection()
+        if want_shortcut:
+            self.skip_connection = SkipConnection()
+        else:
+            self.skip_connection = None
+
         self.sequential = nn.Sequential(
-            LookUpTable(),
-            FirstConvLayer(self.skip_connection, want_shortcut)
+            LookUpTable(num_embedding=69, embedding_dim=16),
+            FirstConvLayer(in_channels=16, out_channels=64, kernel_size=3, skip_connection=self.skip_connection)
         )
         for x in range(len(num_conv_block)):
             for _ in range(num_conv_block[x]):
-                self.sequential.append(ConvolutionalBlock(channels[x], channels[x], self.skip_connection, want_shortcut))
+                self.sequential.append(ConvolutionalBlock(channels[x], channels[x], self.skip_connection))
             self.sequential.append(nn.MaxPool1d(kernel_size=3, stride=2, padding=1))
 
-        self.sequential.append(FullyConnectedBlock(10))
+        self.fc = FullyConnectedBlock(10)
 
     def forward(self, x):
-        return self.sequential(x)
+        out = self.sequential(x)
+        if self.skip_connection is not None:
+            self.skip_connection.clean()
+        return self.fc(out)
 
 
 if __name__ == "__main__":
@@ -142,4 +134,3 @@ if __name__ == "__main__":
     model = VDCNN(depth=9)
     model.eval().to(device)
     summary(model)
-    #print(sum(p.numel() for p in model.parameters() if p.requires_grad))
